ccaa8e573aab3d26e388c434f12429bf6646a6de
matteokjh <429797371@qq.com>
2020-05-11 20:07:59 +0800

    fixed:文章编辑页-正文修改会清空标题

--- src/pages/EditArticle.tsx
+++ src/pages/EditArticle.tsx
@@ -1,38 +1,38 @@
-import React, { useEffect, useState, useContext } from 'react'
-import '@/pages/styles/EditArticle.sass'
-import { Button, Input, message, Tooltip, Form } from 'antd'
-import { InfoCircleOutlined } from '@ant-design/icons'
-import { useHistory } from 'react-router-dom'
-import MarkdownEditor from '@/components/ReactMd/MarkdownEditor'
-import { ArticleProps } from '@/types/articles'
-import { saveArticle, getUserArticle } from '@/api/article'
-import { store } from '@/store'
+import React, { useEffect, useState, useContext } from 'react';
+import '@/pages/styles/EditArticle.sass';
+import { Button, Input, message, Tooltip, Form } from 'antd';
+import { InfoCircleOutlined } from '@ant-design/icons';
+import { useHistory } from 'react-router-dom';
+import MarkdownEditor from '@/components/ReactMd/MarkdownEditor';
+import { ArticleProps } from '@/types/articles';
+import { saveArticle, getUserArticle } from '@/api/article';
+import { store } from '@/store';
 
 const EditArticle = (props: any) => {
-    const history = useHistory()
-    const [articleInfo, setArticleInfo] = useState({} as ArticleProps)
-    const [articleId, setArticleId] = useState('')
-    const [mdContent, setMdContent] = useState('') // 右边的真实data
-    const [form] = Form.useForm()
-    const { userInfo } = useContext(store).state
+    const history = useHistory();
+    const [articleInfo, setArticleInfo] = useState({} as ArticleProps);
+    const [articleId, setArticleId] = useState('');
+    const [mdContent, setMdContent] = useState(''); // 右边的真实data
+    const [form] = Form.useForm();
+    const { userInfo } = useContext(store).state;
 
     // methods
     const goBack = () => {
-        if(articleId) {
-            history.push(`/user/${userInfo.username}/articles/post`)
+        if (articleId) {
+            history.push(`/user/${userInfo.username}/articles/post`);
         } else {
-            history.push(`/`)
+            history.push(`/`);
         }
-    }
+    };
     // 保存
     const saveMd = async () => {
-        let title, subTitle
+        let title, subTitle;
         try {
-            let values = await form.validateFields()
-            title = values.title
-            subTitle = values.subTitle
+            let values = await form.validateFields();
+            title = values.title;
+            subTitle = values.subTitle;
         } catch (err) {
-            return
+            return;
         }
         try {
             let res = await saveArticle({
@@ -41,50 +41,51 @@ const EditArticle = (props: any) => {
                 content: mdContent,
                 title,
                 subTitle,
-            })
+            });
             if (res.code === 200) {
-                message.success(res.msg)
+                message.success(res.msg);
                 // 如果是在 /write，跳转到 /write/:aid
                 if (!articleId) {
-                    history.push(`/write/${res.data.aid}`)
+                    history.push(`/write/${res.data.aid}`);
                 }
             } else {
-                message.error(res.msg)
+                message.error(res.msg);
             }
         } catch (err) {
-            message.error(err)
+            message.error(err);
         }
-    }
+    };
+
     useEffect(() => {
-        setArticleId(props.match.params.aid)
-    }, [props.match.params.aid])
+        setArticleId(props.match.params.aid);
+    }, [props.match.params.aid]);
+
     // 如果是编辑，获取文章数据
     useEffect(() => {
         if (articleId) {
             // 请求
-            ;(async () => {
+            (async () => {
                 try {
-                    let res = await getUserArticle(articleId)
+                    let res = await getUserArticle(articleId);
                     if (res.code === 200) {
-                        const article = res.data
-                        setArticleInfo(article)
-                        setMdContent(article.content || '')
+                        const article = res.data;
+                        setArticleInfo(article);
+                        setMdContent(article.content || '');
+
+                        form.setFieldsValue({
+                            title: article.title,
+                            subTitle: article.subTitle,
+                        });
                     } else {
-                        message.error(res.msg)
+                        message.error(res.msg);
                     }
                 } catch (err) {
-                    message.error(err)
+                    message.error(err);
                 }
-            })()
+            })();
         }
-    }, [articleId])
-    useEffect(() => {
-        console.log(articleInfo)
-        form.setFieldsValue({
-            title: articleInfo.title,
-            subTitle: articleInfo.subTitle,
-        })
-    }, [articleInfo, form])
+        // eslint-disable-next-line
+    }, [articleId]);
 
     return (
         <div className="EditArticle">
@@ -146,7 +147,7 @@ const EditArticle = (props: any) => {
                 ></MarkdownEditor>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default EditArticle
+export default EditArticle;
